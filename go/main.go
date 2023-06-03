056f64541e30bd8b6acb9092962e91cdc3e83ecf
Bisina Wickremasinghe <bisina@vetstoria.com>
2023-06-04 01:34:15 +0530

    readme update, comments and finetune functionality

--- go/main.go
+++ go/main.go
@@ -10,6 +10,7 @@ import (
 	"time"
 )
 
+// Set the struct for the processed data
 type processedTimeZoneData struct {
 	timezone     string
 	variationPST float64
@@ -19,6 +20,7 @@ type processedTimeZoneData struct {
 
 func main() {
 
+	// Get the primary timezones
 	pstTZ, _ := time.LoadLocation("US/Pacific")
 	pstTimezone := time.Date(time.Now().Year(), time.Now().Month(), 0, 0, 0, 0, 0, pstTZ)
 	fmt.Printf("PST Time: %s\n", pstTimezone)
@@ -27,6 +29,7 @@ func main() {
 	estTimezone := time.Date(time.Now().Year(), time.Now().Month(), 0, 0, 0, 0, 0, estTZ)
 	fmt.Printf("EST Time: %s\n", estTimezone)
 
+	// Read the source data CSV file
 	f, err := os.Open("./data/timedata.csv")
 	if err != nil {
 		log.Fatal(err)
@@ -46,12 +49,16 @@ func main() {
 			log.Fatal(err)
 		}
 
+		// Compare the timezones for each row in the CSV file
+		// Add the processed data to the array
 		csv_processed_data = compareTimezones(rec, pstTimezone, estTimezone, csv_processed_data)
 	}
 
+	// Call the function to write the processed data to a new CSV file
 	writeProcessedData(csv_processed_data)
 }
 
+// Write processed data to a new CSV file
 func writeProcessedData(csv_processed_data []processedTimeZoneData) {
 	file, err := os.Create("./processed_data/timezone_data.csv")
 	if err != nil {
@@ -60,9 +67,9 @@ func writeProcessedData(csv_processed_data []processedTimeZoneData) {
 	}
 	defer file.Close()
 
-	w := csv.NewWriter(file)
-	defer w.Flush()
-
+	csvFileWriter := csv.NewWriter(file)
+	defer csvFileWriter.Flush()
+	// # Write the header row
 	csvHeader := []string{"Timezone", "Variation to PST", "Variation to EST", "Comparison"}
 	var csvData [][]string
 	csvData = append(csvData, csvHeader)
@@ -70,20 +77,25 @@ func writeProcessedData(csv_processed_data []processedTimeZoneData) {
 	for _, record := range csv_processed_data {
 		csvData = append(csvData, []string{record.timezone, fmt.Sprint(record.variationPST), fmt.Sprint(record.variationEST), record.comparison})
 	}
-	w.WriteAll(csvData)
+	csvFileWriter.WriteAll(csvData)
 }
 
+// Compare timezone data
 func compareTimezones(rec []string, pstTimezone time.Time, estTimezone time.Time, csv_processed_data []processedTimeZoneData) []processedTimeZoneData {
+	// Load the timezone to check
 	tzToCheck, _ := time.LoadLocation(rec[0])
 	tzCompareLocalized := time.Date(time.Now().Year(), time.Now().Month(), 0, 0, 0, 0, 0, tzToCheck)
-	fmt.Printf("Compared Time: %s\n", tzCompareLocalized)
+	fmt.Printf("Compared Timezone Time: %s\n", tzCompareLocalized)
 
+	// Compare against PST
 	comparePST := math.Abs(float64((pstTimezone.Sub(tzCompareLocalized).Minutes())))
+	fmt.Printf("Variation to PST Time: %v\n", comparePST)
+
+	// Compare against EST
 	compareEST := math.Abs(float64(estTimezone.Sub(tzCompareLocalized).Minutes()))
+	fmt.Printf("Variation to EST Time: %v\n", compareEST)
 
-	fmt.Printf("PST: %v\n", comparePST)
-	fmt.Printf("EST: %v\n", compareEST)
-
+	// Compare the PST vs EST
 	comparison := ""
 	if comparePST > compareEST {
 		comparison = "Closer to EST"
@@ -93,6 +105,7 @@ func compareTimezones(rec []string, pstTimezone time.Time, estTimezone time.Time
 		comparison = "No difference can be used either in EST or PST"
 	}
 
+	// Add to the slice created by the struct
 	csv_processed_data = append(csv_processed_data, processedTimeZoneData{
 		rec[0], comparePST, compareEST, comparison})
 	return csv_processed_data
--- python/main.py
+++ python/main.py
@@ -1,49 +1,85 @@
 from datetime import datetime
 import pytz
 import csv
+import os
 
+# Compare timezone data
 def compareTimezones(inputDatetime, tzPST, tzEST, rowData):
-	tzToCheck = pytz.timezone(str(rowData[0]))
-	print(f'Timezone to Compare: {tzToCheck}')
-	tzCompareLocalized = tzToCheck.localize(inputDatetime)
-	print(f'Timezone Time: {tzCompareLocalized}')
+	try:
+		# Get timezone to compare
+		tzToCheck = pytz.timezone(str(rowData[0]))
+		print(f'Timezone to Compare: {tzToCheck}')
+		tzCompareLocalized = tzToCheck.localize(inputDatetime)
+		print(f'Compared Timezone Time: {tzCompareLocalized}')
 
-	comparePST = abs((tzPST - tzCompareLocalized).total_seconds()/3600)
-	print(f'Variation to PST Time: {comparePST}')
-	compareEST = abs((tzEST - tzCompareLocalized).total_seconds()/3600)
-	print(f'Variation to EST Time: {compareEST}')
+		# Compare against PST
+		comparePST = abs((tzPST - tzCompareLocalized).total_seconds()/3600)
+		print(f'Variation to PST Time: {comparePST}')
 
-	comparison = "None"
-	if comparePST > compareEST:
-		comparison = "Closer to EST"
-	elif comparePST < compareEST:
-		comparison = "Closer to PST"
-	else:
-		comparison = "No difference can be used either in EST or PST"
+		# Compare against EST
+		compareEST = abs((tzEST - tzCompareLocalized).total_seconds()/3600)
+		print(f'Variation to EST Time: {compareEST}')
 
-	csv_processed_data = [rowData[0], comparePST, compareEST, comparison]
-	return csv_processed_data 
+		# Compare the PST vs EST
+		comparison = "None"
+		if comparePST > compareEST:
+			comparison = "Closer to EST"
+		elif comparePST < compareEST:
+			comparison = "Closer to PST"
+		else:
+			comparison = "No difference can be used either in EST or PST"
 
-def writeProcessedData(data):
-    with open('./python/processed_data/timezone_data.csv', 'w', newline='') as csv_file:
-     csv_writer = csv.writer(csv_file, delimiter=',')
-     csv_writer.writerow(['Timezone', 'Variation to PST', 'Variation to EST', 'Comparison'])
-     csv_writer.writerows(data)
-
-pstTZ = pytz.timezone('US/Pacific')
-estTZ = pytz.timezone('US/Eastern')
-inputDatetime = datetime.now()
-print(f'Current Time: {inputDatetime}')
-pstTimezone = pstTZ.localize(inputDatetime)
-print(f'PST Time: {pstTimezone}')
-estTimezone = estTZ.localize(inputDatetime)
-print(f'EST Time: {estTimezone}')
-
-timezoneData = []
-with open('./python/data/timedata.csv') as csv_file:
-	csv_reader = csv.reader(csv_file, delimiter=',')
+		# Add to the array
+		csv_processed_data = [rowData[0], comparePST, compareEST, comparison]
+		return csv_processed_data
 	
-	for rowData in csv_reader:
-		timezoneData.append(compareTimezones(inputDatetime, pstTimezone, estTimezone, rowData))
+	except Exception as err:
+		print(f'Error Occurred: {err}')
+	
+# Write processed data to a new CSV file
+def writeProcessedData(data):
+	try:
+		with open(f'{os.getcwd()}/python/processed_data/timezone_data.csv', 'w', newline='') as csv_file:
+			csv_writer = csv.writer(csv_file, delimiter=',')
+			# Write the header row
+			csv_writer.writerow(['Timezone', 'Variation to PST', 'Variation to EST', 'Comparison'])
+			csv_writer.writerows(data)
+	except Exception as err:
+		print(f'Error Occurred: {err}')
 
-writeProcessedData(timezoneData)
+
+def main():
+
+	# Set primary timezones
+	pstTZ = pytz.timezone('US/Pacific')
+	estTZ = pytz.timezone('US/Eastern')
+	inputDatetime = datetime.now()
+
+	# Get the time in the required timezones
+	print(f'Current Time: {inputDatetime}')
+	pstTimezone = pstTZ.localize(inputDatetime)
+	print(f'PST Time: {pstTimezone}')
+	estTimezone = estTZ.localize(inputDatetime)
+	print(f'EST Time: {estTimezone}')
+
+	try:
+		timezoneData = []
+
+		# Read the source CSV file
+		with open(f'{os.getcwd()}/python/data/timedata.csv') as csv_file:
+			csv_reader = csv.reader(csv_file, delimiter=',')
+
+			# Compare the timezones for each row in the CSV file
+			# Add the processed data to the array
+			for rowData in csv_reader:
+				timezoneData.append(compareTimezones(inputDatetime, pstTimezone, estTimezone, rowData))
+
+		# Call the function to write the processed data to a new CSV file
+		writeProcessedData(timezoneData)
+
+	except Exception as err:
+		print(f'Error Occurred: {err}')
+  
+  
+if __name__=="__main__":
+    main()
